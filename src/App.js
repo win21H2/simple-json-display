3a9a4b4156430cf891f27fa87a9e9f48621472e9
324Hz <mark@cosimo.funds>
2022-07-03 13:34:49 -0500

    commit (before it breaks)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Users from './useUsers'
 
-
 function App() {
   return (
     <div className="App">
-      <h1 align="center">Github API list example</h1>
+      <h1 align="center">Simple JSON display</h1>
       <Users />
     </div>
   );
--- src/useUsers.js
+++ src/useUsers.js
@@ -6,14 +6,21 @@ function Users() {
   return (
       <div>
           {data && (
-            data.map((user) =>(
-                <div className="text-white" key={user.id}>
+            data.map((something) =>(
+                <div className="text-white" key={something.id}>
                     <div className="border">
-                        <p>User: {user.login} </p>
-                        <p>Login type: { user.type } </p>
-                        <p>Id: { user.id }</p>
-                        <img src={user.avatar_url} alt="avatar" className="github--Userpfp"/>
+                        <br />
+                        <p className="center">User: {something.login} </p>
+                        <p className="center">Login type: { something.type } </p>
+                        <p className="center">Id: { something.id }</p>
+                        <p className="center">Gravatar id: { something.gravatar_id }</p>
+                        <p className="center">Node id: { something.node_id }</p>
+                        <a className="center" href={ something.avatar_url } rel="noreferrer" target="_blank"><img src={ something.avatar_url } alt="avatar" className="github--Userpfp"/></a>
                         <br /><br />
+                        {/*
+                        WHAT TO DO NEXT:
+                         - Try to make it so that there is a URL inputter and when someone sends a URL, it creates a data.map and then displays all of the JSON data from that site in a JSONpretty way
+                         */}
                     </div>
                 </div>
             ))
